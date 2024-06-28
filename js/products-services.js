59d8631ee0228c65b3223cb688f96d1432b7e1f7
Nicole Leyva <leyvanm217@gmail.com>
2024-06-27 18:55:25 -0600

    changes

--- js/products-services.js
+++ js/products-services.js
@@ -1,34 +1,32 @@
-const productosList=()=>{
+const productosList = () => {
     return fetch("http://localhost:3001/items")
-    .then((res)=>res.json())
-    .catch((err)=>console.log(err));
-};
-
-const createProductos=( Nombre,Precio,image)=>{
-    return fetch("http://localhost:3001/items",{
-method:"POST",
-headers:{
-    "Content-Type":"application/json",
-},
-body:JSON.stringify({
-
-Nombre,
-Precio,
-image,
-
-})
-    }).then((res)=>res.json()).catch((err)=>console.log(err));
-}
-
-const deleteProducto = (id) => {
+      .then((res) => res.json())
+      .catch((err) => console.log(err));
+  };
+  
+  const createProductos = (name, price, image_url) => {
+    return fetch("http://localhost:3001/items", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        name,
+        price,
+        image_url,
+      }),
+    }).then((res) => res.json()).catch((err) => console.log(err));
+  }
+  
+  const deleteProducto = (id) => {
     return fetch(`http://localhost:3001/items/${id}`, {
-        method: "DELETE",
+      method: "DELETE",
     }).catch((err) => console.log(err));
-}; 
-    
-
-export const servicesProductos={
+  }; 
+  
+  export const servicesProductos = {
     productosList,
     createProductos,
     deleteProducto,
-};
+  };
+  
--- main.js
+++ main.js
@@ -1,63 +1,65 @@
 import { servicesProductos } from "./js/products-services.js";
 
-const listaCard= document.querySelector("[data-product]");
-const form=document.querySelector("[data-form]")
+const listaCard = document.querySelector("[data-product]");
+const form = document.querySelector("[data-form]");
 
-function createCard(id,Nombre, Precio, image){
-
-   const card=document.createElement("div");
-   card.classList.add("card");
-   card.innerHTML=
-   `<div class="imagen-container">
-           <img src="${image}" alt="${Nombre}">
-            <div class="informacion">
-                <p class="car-titulo">"${Nombre}"</p>  
-            </div>
-            <div class="value">
-                <p>$ ${Precio}</p> 
-                <button class="delete-button"data-id="${id}">
-                <img class="borrar" src="./assets/trash.svg" data-remove="true">
-            </button>
-            </div>
-        </div>`
-        ;
-        const deleteButton = card.querySelector(".delete-button");
-deleteButton.addEventListener("click", () => {
+function createCard(id, name, price, image_url) {
+  const card = document.createElement("div");
+  card.classList.add("card");
+  card.innerHTML = `
+    <div class="imagen-container">
+      <img src="${image_url}" alt="${name}">
+      <div class="informacion">
+        <p class="car-titulo">${name}</p>  
+      </div>
+      <div class="value">
+        <p>$ ${price}</p> 
+        <button class="delete-button" data-id="${id}">
+          <img class="borrar" src="./assets/trash.svg" data-remove="true">
+        </button>
+      </div>
+    </div>
+  `;
+  const deleteButton = card.querySelector(".delete-button");
+  deleteButton.addEventListener("click", () => {
     servicesProductos.deleteProducto(id).then(() => {
-        card.remove();
+      card.remove();
     }).catch((err) => console.log(err));
-}); 
+  });
 
-        listaCard.appendChild(card);
-        return card;
+  listaCard.appendChild(card);
+  return card;
 }
 
-
-
-
-const render= async()=>{
-    try{
-        const listProduc=await servicesProductos.productosList(); 
-listProduc.forEach(productos => {
-    listaCard.appendChild(createCard(
+const render = async () => {
+  try {
+    const listProduc = await servicesProductos.productosList();
+    listProduc.forEach(productos => {
+      listaCard.appendChild(createCard(
         productos.id,
-        productos.Nombre,
-        productos.Precio,
-        productos.image
-    ))
-});
-    }catch(error){
-        console.log(error)
-    }
-
+        productos.name,
+        productos.price,
+        productos.image_url
+      ));
+    });
+  } catch (error) {
+    console.log(error);
+  }
 };
 
-form.addEventListener("submit", (event)=>{
-    event.preventDefault();
-    const name= document.querySelector("[data-name]").value;
-    const price= document.querySelector("[data-price]").value;
-    const image= document.querySelector("[data-image]").value;
-    
-    servicesProductos.createProductos(name,price,image).then((res)=>console.log(res)).catch((err)=>console.log(err))
-})
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const name = document.querySelector("[data-name]").value;
+  const price = document.querySelector("[data-price]").value;
+  const image_url = document.querySelector("[data-image]").value;
+
+  servicesProductos.createProductos(name, price, image_url)
+    .then((res) => {
+      console.log(res);
+      // Podrías agregar la nueva tarjeta aquí también si lo deseas
+      render(); // Renderizar de nuevo para incluir el nuevo producto
+    })
+    .catch((err) => console.log(err));
+});
+
 render();
